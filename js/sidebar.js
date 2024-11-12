ae7df1005bdbd944026b0223c0b66f308cc2b3d9
phamvana <72136536+phamvana@users.noreply.github.com>
2024-11-12 14:40:46 +0700

    cập nhật 12-11-24

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,14 +3,14 @@ const sidebarTitleEl = document.getElementById('sidebar-title');
 let isHiddenLesson = true;
 const sidebarEl = document.getElementById('sidebar');
 const lessonList = document.getElementById('lessonList');
-lessonList.style.display = "none";
+lessonList.style.display = "block";
 sidebarTitleEl.addEventListener('click', function () {
     sidebarEl.classList.toggle('active');
     if(isHiddenLesson){
         isHiddenLesson = !isHiddenLesson;
         lessonList.style.display = "block";
     }else{
+        sidebarEl.classList.toggle('active');
         isHiddenLesson = !isHiddenLesson;
-        lessonList.style.display = "none";
     }
 });
